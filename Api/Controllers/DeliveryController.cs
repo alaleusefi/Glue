f7bef1395747f5e32acafca81471bef48023eb84
ally <alaleusefi@yahoo.com>
2021-05-16 20:17:50 +0100

    Cleanup

--- Api/Controllers/DeliveryController.cs
+++ Api/Controllers/DeliveryController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Repositories;
-using API.Request;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Model;
